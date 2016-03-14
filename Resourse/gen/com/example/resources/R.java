a2ab7b417b7ce478f66e4eb40de2e4d3218dd12b
YC-YC <yellowc.yc@aliyun.com>
2016-03-14 19:56:10 +0800

    添加socket功能

--- Resourse/gen/com/example/resources/R.java
+++ Resourse/gen/com/example/resources/R.java
@@ -64,10 +64,14 @@ public final class R {
         public static final int shape=0x7f02001e;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
+        public static final int action_settings=0x7f090013;
         public static final int age=0x7f090007;
+        public static final int btn_client1_send=0x7f09000e;
+        public static final int btn_client2_send=0x7f090012;
         public static final int btn_contentprovider=0x7f090000;
         public static final int btn_insert=0x7f090009;
+        public static final int et_client1=0x7f09000d;
+        public static final int et_client2=0x7f090011;
         public static final int id=0x7f090005;
         public static final int image=0x7f090003;
         public static final int imageview_lamp=0x7f090004;
@@ -76,6 +80,10 @@ public final class R {
         public static final int img_tween=0x7f090002;
         public static final int listView_content=0x7f09000a;
         public static final int name=0x7f090006;
+        public static final int scroll2_content=0x7f09000f;
+        public static final int scroll_content=0x7f09000b;
+        public static final int tv_client1_content=0x7f09000c;
+        public static final int tv_client2_content=0x7f090010;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -91,14 +99,19 @@ public final class R {
         public static final int resolver_main=0x7f03000a;
         public static final int resource_main=0x7f03000b;
         public static final int shape=0x7f03000c;
+        public static final int socket_client1=0x7f03000d;
+        public static final int socket_client2=0x7f03000e;
     }
     public static final class menu {
         public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
+        public static final int action_settings=0x7f060004;
         public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060002;
+        public static final int hello_world=0x7f060005;
+        public static final int socket_client1=0x7f060002;
+        public static final int socket_client2=0x7f060003;
+        public static final int socket_server=0x7f060001;
     }
     public static final class style {
         public static final int AnimationActivity=0x7f070003;
